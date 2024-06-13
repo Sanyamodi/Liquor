3c3a5ae2057232de038e30dc0025c4a187ba8885
Krishna Jadhav <krishnajadhav85017@gmail.com>
2024-06-14 04:17:55 +0530

    middle ware banan hai

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,32 +3,15 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const userModel = require("../models/user-model");
+const { setToken } = require("../utils/setToken");
+const {registerUser,loginUser} = require("../controlers/authControler");
 
 router.get("/", (req, res) => {
     res.send("hey from user");
 });
 
-router.post("/register", (req, res) => {
-    try {
-        let { fullname, email, password } = req.body;
-        bcrypt.genSalt(11, function (err, salt) {
-            bcrypt.hash(password, salt, async function (err, hash) {
-                if(err) return res.send(err.message);
-                else{
-                    let user = await userModel.create({
-                        fullname,
-                        email,
-                        password:hash,
-                    });
-                    let token = jwt.sign({email,id:user._id}, process.env.JWT_SECRET);
-                    res.cookie("token",token);
-                    res.send("ho gya create");
-                }
-            });
-        });
-    } catch (err) {
-        res.send(err.message);
-    }
-});
+router.post("/register",registerUser);
+
+router.post("/login",loginUser);
 
 module.exports = router;
