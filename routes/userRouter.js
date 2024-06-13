bfd739e21e60a6a19315b2a8344a71ab213bc472
Krishna Jadhav <krishnajadhav85017@gmail.com>
2024-06-14 01:19:20 +0530

    auth ho gya

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const userModel = require("../models/user-model");
 
-router.get('/',(req,res)=>{
-    res.send("hey from user")
-})
+router.get("/", (req, res) => {
+    res.send("hey from user");
+});
 
-module.exports = router
+router.post("/register", (req, res) => {
+    try {
+        let { fullname, email, password } = req.body;
+        bcrypt.genSalt(11, function (err, salt) {
+            bcrypt.hash(password, salt, async function (err, hash) {
+                if(err) return res.send(err.message);
+                else{
+                    let user = await userModel.create({
+                        fullname,
+                        email,
+                        password:hash,
+                    });
+                    let token = jwt.sign({email,id:user._id}, process.env.JWT_SECRET);
+                    res.cookie("token",token);
+                    res.send("ho gya create");
+                }
+            });
+        });
+    } catch (err) {
+        res.send(err.message);
+    }
+});
+
+module.exports = router;
