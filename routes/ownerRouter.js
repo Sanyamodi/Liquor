1de4f977df3534142d330303bcc18509277e77db
Sanya Modi <modisanya05@gmail.com>
2024-10-03 14:09:44 +0530

    changes hai , name add kiya hai Auth  me

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -14,6 +14,7 @@ if (process.env.NODE_ENV === "development") {
                 fullname,
                 email,
                 password,
+                name
             });
             res.status(201).send(createOwner);
         } catch (err) {
